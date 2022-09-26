775ccbbbbc08607cada3a9788de85ba110bc0e2d
Logan <logan70@163.com>
2022-09-26 09:52:35 +0800

    feat: [medium] fibonacci sequence

---
+++ solutions/medium-fibonacci-sequence.ts
@@ -0,0 +1,46 @@
+/*
+  4182 - Fibonacci Sequence
+  -------
+  by windliang (@wind-liang) #medium 
+  
+  ### Question
+  
+  Implement a generic `Fibonacci<T>` that takes a number `T` and returns its corresponding [Fibonacci number](https://en.wikipedia.org/wiki/Fibonacci_number).
+  
+  The sequence starts:
+  1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+  
+  For example
+  ```ts
+  type Result1 = Fibonacci<3> // 2
+  type Result2 = Fibonacci<8> // 21
+  ```
+  
+  > View on GitHub: https://tsch.js.org/4182
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type Fibonacci<T extends number, Prev extends any[] = [], Current extends any[] = [any], Depth extends any[] = [any]> = T extends Depth['length']
+    ? Current['length']
+    : Fibonacci<T, Current, [...Prev, ...Current], [...Depth, any]>;
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Fibonacci<3>, 2>>,
+  Expect<Equal<Fibonacci<8>, 21>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4182/answer
+  > View solutions: https://tsch.js.org/4182/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
