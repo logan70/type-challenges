85ed74498ecad27a2b396bfa7a2275244d0a8810
Logan <logan70@163.com>
2022-10-19 09:45:27 +0800

    feat: [hard] union to intersection

---
+++ solutions/hard-union-to-intersection.ts
@@ -0,0 +1,46 @@
+/*
+  55 - Union to Intersection
+  -------
+  by Zheeeng (@zheeeng) #hard #utils #infer
+  
+  ### Question
+  
+  Implement the advanced util type `UnionToIntersection<U>`
+  
+  For example
+  
+  ```ts
+  type I = Union2Intersection<'foo' | 42 | true> // expected to be 'foo' & 42 & true
+  ```
+  
+  > View on GitHub: https://tsch.js.org/55
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2)
+  ? true
+  : false;
+
+type UnionToIntersection<U> = (U extends any ? (a: U) => any : never) extends ((a: infer R) => any)
+  ? R
+  : never
+
+/* _____________ Test Cases _____________ */
+import type { Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<UnionToIntersection<'foo' | 42 | true>, 'foo' & 42 & true>>,
+  Expect<Equal<UnionToIntersection<(() => 'foo') | ((i: 42) => true)>, (() => 'foo') & ((i: 42) => true)>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/55/answer
+  > View solutions: https://tsch.js.org/55/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
