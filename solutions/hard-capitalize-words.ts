119f00c1ca3c25f00f7daf2de4f4171131d73365
Logan <logan70@163.com>
2022-10-25 10:10:04 +0800

    feat: [hard] capitalize words

---
+++ solutions/hard-capitalize-words.ts
@@ -0,0 +1,57 @@
+/*
+  112 - Capitalize Words
+  -------
+  by Anthony Fu (@antfu) #hard #template-literal
+  
+  ### Question
+  
+  Implement `CapitalizeWords<T>` which converts the first letter of **each word of a string** to uppercase and leaves the rest as-is.
+  
+  For example
+  
+  ```ts
+  type capitalized = CapitalizeWords<'hello world, my friends'> // expected to be 'Hello World, My Friends'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/112
+*/
+
+import { Equal } from '@type-challenges/utils'
+
+
+/* _____________ Your Code Here _____________ */
+
+type Alphabet = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G'
+  | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P'
+  | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z'
+
+type CapitalizeWords<S extends string, Prev extends string = '', Result extends string = ''> = S extends `${infer F}${infer R}`
+  ? Uppercase<Prev> extends Alphabet
+    ? CapitalizeWords<R, F, `${Result}${F}`>
+    : CapitalizeWords<R, F, `${Result}${Uppercase<F>}`>
+  : Result;
+
+
+type test = CapitalizeWords<'aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq'>
+/* _____________ Test Cases _____________ */
+import type { Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CapitalizeWords<'foobar'>, 'Foobar'>>,
+  Expect<Equal<CapitalizeWords<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<CapitalizeWords<'foo bar'>, 'Foo Bar'>>,
+  Expect<Equal<CapitalizeWords<'foo bar hello world'>, 'Foo Bar Hello World'>>,
+  Expect<Equal<CapitalizeWords<'foo bar.hello,world'>, 'Foo Bar.Hello,World'>>,
+  Expect<Equal<CapitalizeWords<'aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq'>, 'Aa!Bb@Cc#Dd$Ee%Ff^Gg&Hh*Ii(Jj)Kk_Ll+Mm{Nn}Oo|Pp🤣Qq'>>,
+  Expect<Equal<CapitalizeWords<''>, ''>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/112/answer
+  > View solutions: https://tsch.js.org/112/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
